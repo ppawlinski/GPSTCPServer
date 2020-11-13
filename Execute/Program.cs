75c72fc1b0f1d93380bcaef93eb49f47679403bb
Jan Majewski <jan.majewski@student.put.poznan.pl>
2020-11-13 23:09:41 +0100

    Klasa database implementacja wg idisposable

--- Execute/Program.cs
+++ Execute/Program.cs
@@ -15,7 +15,7 @@ namespace Execute
     {
         static void Main(string[] args)
         {
-            Database database = new Database();
+            Database database = new Database("Data Source=database.sqlite3");
             GPSTCPServer.TCPServerGPS server = new TCPServerGPS(IPAddress.Any, 2048, database);
             server.RunServer();
             while (true) ;
