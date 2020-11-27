8cec43c8c53b34b52c0e790737792e2adc9e9f7a
patryk.pawlinski <patryk.pawlinski@student.put.poznan.pl>
2020-11-27 14:50:39 +0100

    usunięcie testowej zmiennej

--- GPSTCPServer/GPSUser.cs
+++ GPSTCPServer/GPSUser.cs
@@ -6,18 +6,14 @@ namespace GPSTCPServer
     internal class GPSUser
     {
         public TcpClient client;
-
-        public int random;
         public bool LoggedIn { get; set; }
         public string Username { get; set; }
 
         public GPSUser(TcpClient client)
         {
-            Random rand = new Random();
             this.client = client;
             LoggedIn = false;
             Username = null;
-            random = rand.Next();
         }
 
     }
